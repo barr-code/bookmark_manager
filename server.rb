ce8bdfbcf1b1f95946da47ae322ce0348da4fa82
Victoria Barr <vb329@georgetown.edu>
2014-10-07 17:38:57 +0100

    Won't let user sign up if email is already registered.

--- server.rb
+++ server.rb
@@ -26,9 +26,7 @@ end
 post '/links' do
 	url = params[:url]
 	title = params[:title]
-	tags = params[:tags].split(' ').map do |tag|
-		Tag.first_or_create(:text => tag)
-	end
+	tags = params[:tags].split(' ').map {|tag| Tag.first_or_create(:text => tag)}
 	Link.create(:url => url, :title => title, :tags => tags)
 	redirect to('/')
 end
