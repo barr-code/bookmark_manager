4fbca56b225fbb53c8a9c96bec35e6f78c682f82
Victoria Barr <vb329@georgetown.edu>
2014-10-08 14:29:50 +0100

    Fixed some error redundancies and password confirmation box

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -33,7 +33,7 @@ feature "User signs up" do
 	scenario "with an email that is already registered" do 
 		expect {sign_up}.to change(User, :count).by(1)
 		expect {sign_up}.to change(User, :count).by(0)
-		expect(page).to have_content("This email is already taken.")
+		expect(page).to have_content("This email is already taken")
 	end
 end
 
