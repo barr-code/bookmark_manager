858b4f8031a2a4ea09c0f5ff6b4af5ff4cea12bd
Victoria Barr <vb329@georgetown.edu>
2014-10-08 11:31:41 +0100

    Authenticates user Signin

--- server.rb
+++ server.rb
@@ -1,63 +1,72 @@
 require 'sinatra'
 require 'data_mapper'
 require 'rack-flash'
-
-env = ENV["RACK_ENV"] || "development"
-
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
-
 require './lib/link'
 require './lib/tag'
 require './lib/user'
+require './lib/data_mapper_setup'
 
-DataMapper.finalize
-
-DataMapper.auto_upgrade!
 
 enable :sessions
 set :session_secret, 'super secret'
 use Rack::Flash
 
-get '/' do 
-	@links = Link.all
-	erb :index
-end
+	get '/' do 
+		@links = Link.all
+		erb :index
+	end
 
-post '/links' do
-	url = params[:url]
-	title = params[:title]
-	tags = params[:tags].split(' ').map {|tag| Tag.first_or_create(:text => tag)}
-	Link.create(:url => url, :title => title, :tags => tags)
-	redirect to('/')
-end
-
-get '/tags/:text' do
-	tag = Tag.first(:text => params[:text])
-	@links = tag ? tag.links : []
-	erb :index
-end
-
-get '/users/new' do 
-	@user = User.new
-	erb :"users/new"
-end
-
-post '/users' do 
-	@user = User.create(:email => params[:email],
-		:password => params[:password],
-		:password_confirmation => params[:password_confirmation])
-	
-	if @user.save
-		session[:user_id] = @user.id
+	post '/links' do
+		url = params[:url]
+		title = params[:title]
+		tags = params[:tags].split(' ').map {|tag| Tag.first_or_create(:text => tag)}
+		Link.create(:url => url, :title => title, :tags => tags)
 		redirect to('/')
-	else
-		flash.now[:errors] = @user.errors.full_messages
+	end
+
+	get '/tags/:text' do
+		tag = Tag.first(:text => params[:text])
+		@links = tag ? tag.links : []
+		erb :index
+	end
+
+	get '/users/new' do 
+		@user = User.new
 		erb :"users/new"
 	end
-end
 
-helpers do 
-	def current_user
-		@current_user ||=User.get(session[:user_id]) if session[:user_id]
+	post '/users' do 
+		@user = User.create(:email => params[:email],
+			:password => params[:password],
+			:password_confirmation => params[:password_confirmation])
+		
+		if @user.save
+			session[:user_id] = @user.id
+			redirect to('/')
+		else
+			flash.now[:errors] = @user.errors.full_messages
+			erb :"users/new"
+		end
 	end
-end
+
+	get '/sessions/new' do 
+		erb :"sessions/new"
+	end
+
+	post '/sessions' do
+		email, password = params[:email], params[:password]
+		user = User.authenticate(email, password)
+		if user 
+			session[:user_id] = user.id
+			redirect to('/')
+		else
+			flash[:errors] = ["The email or password is incorrect."]
+			erb :"sessions/new"
+		end
+	end
+
+	helpers do 
+		def current_user
+			@current_user ||=User.get(session[:user_id]) if session[:user_id]
+		end
+	end
